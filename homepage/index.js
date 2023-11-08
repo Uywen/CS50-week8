0454f0a926b1e5a3f3b0eaf4b16a73b8dff68288
Uywen <uywenafrica27@gmail.com>
2023-11-08 20:54:26 +0200

    added a few extra things

--- homepage/index.js
+++ homepage/index.js
@@ -1,5 +1,5 @@
+// An alert that will be triggered once user clicks on the signup button
 function SignUp(){
-
     alert(`The Signup page doesnt work 🤣
 Sorry for wasting 2 seconds of your time.`)
 }
